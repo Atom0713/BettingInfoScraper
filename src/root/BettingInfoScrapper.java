e2ad6f881044d6f230a5e93ede6ce0ed04cec794
Artem Sliusarenko <sliusarenko.artem@gmail.com>
2019-08-07 18:25:24 +0300

    Instead of saving and then writing data now data is written to the sheet directly after it is scrapped.

--- src/root/BettingInfoScrapper.java
+++ src/root/BettingInfoScrapper.java
@@ -20,19 +20,27 @@ public class BettingInfoScrapper {
 //    private static ArrayList<String[]> tennis = new ArrayList<>();
 //    private static ArrayList<String[]> hockey = new ArrayList<>();
     private static XSSFWorkbook workbook = new XSSFWorkbook();
-    //Declare  blank sheet
     private static XSSFSheet sheet;
+    private static final String url = "https://hintwise.com/";
+    //Declare  blank sheet
+
 
 
 
     public static void main(String[] args) {
+        //sheet = workbook.createSheet("Soccer");
         readInformation("#soccer");
-//        System.out.println();
-//        readInformation("#basketball");
-//        System.out.println();
-//        readInformation("#tennis");
-//        System.out.println();
-//        readInformation("#hockey");
+        System.out.println();
+        //sheet = workbook.createSheet("Basketball");
+        readInformation("#basketball");
+        System.out.println();
+        //sheet = workbook.createSheet("Tennis");
+        readInformation("#tennis");
+        System.out.println();
+        //sheet = workbook.createSheet("Hockey");
+        readInformation("#hockey");
+
+        autisizeColumns(workbook,4,4);
 
         try
         {
@@ -46,52 +54,10 @@ public class BettingInfoScrapper {
     }
 
     private static void readInformation(String sportType){
-        System.out.println("Read " + sportType + " data!");
-        ArrayList<String[]> dataHolder = new ArrayList<>();
-        String[] elementRow = new String[4] ;
-        final String url = "https://hintwise.com/";
 
-        try{
-            final Document bettingInfo = Jsoup.connect(url).get();
-
-            for(Element row : bettingInfo.select(sportType +  " table.items tr")){
-
-
-                final String time = row.select(".cellStyle.cell40").text();
-                final String teams = row.select(".cellTEAMS.cellStyle").text();
-                final String division = row.select(".hidden-xs.cellStyle").text();
-
-                String prediction = row.select(".cellStyle.lr20.cell90").text();
-                prediction = prediction.replace("Away", "A");
-                prediction = prediction.replace("Draw", "D");
-                prediction = prediction.replace("Home", "H");
-                elementRow[0] = time;
-                elementRow[1] = teams;
-                elementRow[2] = division;
-                elementRow[3] = prediction;
-                //System.out.println(elementRow[0] + elementRow[1] + elementRow[2] + elementRow[3]);
-
-                dataHolder.add(elementRow);
-
-                //System.out.println(time + "\t" + teams + "\t" + division + "\t" + prediction);
-            }
-            //TODO: dataHolder is filled with the last row of data!!! FIX data storing problem!!!
-
-            //System.out.println(dataHolder.get(20)[1]);
-            extractData(sportType, dataHolder);
-
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
-    }
-
-
-    private static void extractData(String sportType, ArrayList<String[]> dataHolder)
-    {
-        System.out.println("Writing "+ sportType + " data");
-
-        workbook = new XSSFWorkbook();
-
+        /*
+        * Style and font attributes of the excel sheets.
+         */
         XSSFCellStyle headers = workbook.createCellStyle();
         XSSFCellStyle tableStyle = workbook.createCellStyle();
 
@@ -117,60 +83,95 @@ public class BettingInfoScrapper {
         headers.setAlignment(HorizontalAlignment.CENTER);
 
 
-
-        //Declare couple neccessary objects,
+        //Declare couple neccessary objectsand variables,
         Object[] objArr = null;
         Row row = null;
         Cell cell = null;
         int rowNum = 0;
         int cellNum = 0;
 
-        sheet = workbook.createSheet(sportType);
+        //Create sheet named by the type of sport
+        sheet = workbook.createSheet(sportType.replace("#",""));
 
-        for(int rows = 0; rows < dataHolder.size(); rows++){
+        /*
+        * Fill first row of a sheet with column headers
+         */
+        row = sheet.createRow(rowNum++);
+        objArr = new Object[] {"Time", "Home vs Away", "League","Prediction"};
+        for (Object obj : objArr)
+        {
+            cell = row.createCell(cellNum++);
+            cell.setCellStyle(headers);
+            cell.setCellValue((String)obj);
 
-            row = sheet.createRow(rowNum++);
+        }
+        objArr = null;
 
-            if(rowNum == 1)
-            {
-                objArr = new Object[] {"Time", "Home vs Away", "League","Prediction"};
-                for (Object obj : objArr)
-                {
-                    cell = row.createCell(cellNum++);
-                    cell.setCellStyle(headers);
-                    cell.setCellValue((String)obj);
+        System.out.println("Read and write " + sportType.replace("#","") + " data!");
+        String[] elementRow = new String[4] ;
 
-                }
-                objArr = null;
-            }else{
-                objArr = dataHolder.get(rows);
-                //System.out.println(objArr[1]);
-                //break;
-                cellNum = 0;
-                for(Object obj: objArr){
-                    cell = row.createCell(cellNum++);
-                    cell.setCellStyle(tableStyle);
 
-                    if(obj instanceof String)
-                    {
-                        cell.setCellValue((String)obj);
 
-                    } else if(obj instanceof Integer)
-                    {
-                        cell.setCellValue((Integer)obj);
+        try{
+            /*
+            * Extract, store and write scrapped data.
+             */
+            final Document bettingInfo = Jsoup.connect(url).get();
+
+            for(Element rowElement : bettingInfo.select(sportType +  " table.items tr")){
+
+                if(rowElement.select(".cellStyle.cell40").text().equals("")){
+                    //Skips empty row elements at any level of the table.
+                }else{
+                    final String time = rowElement.select(".cellStyle.cell40").text();
+                    final String teams = rowElement.select(".cellTEAMS.cellStyle").text();
+                    final String division = rowElement.select(".hidden-xs.cellStyle").text();
+
+                    String prediction = rowElement.select(".cellStyle.lr20.cell90").text();
+                    prediction = prediction.replace("Away", "A");
+                    prediction = prediction.replace("Draw", "D");
+                    prediction = prediction.replace("Home", "H");
+
+                    elementRow[0] = time;
+                    elementRow[1] = teams;
+                    elementRow[2] = division;
+                    elementRow[3] = prediction;
+
+                    row = sheet.createRow(rowNum++);
+
+                    objArr = elementRow;
+                    cellNum = 0;
+                    for (Object obj : objArr) {
+                        cell = row.createCell(cellNum++);
+                        cell.setCellStyle(tableStyle);
+
+                        if (obj instanceof String) {
+                            cell.setCellValue((String) obj);
+
+                        } else if (obj instanceof Integer) {
+                            cell.setCellValue((Integer) obj);
+                        }
                     }
-
+                    cellNum = 0;
                 }
-                cellNum = 0;
             }
 
 
-
-
-
-
+        }catch (Exception ex){
+            ex.printStackTrace();
         }
-
     }
+
+    public static void autisizeColumns(Workbook workbook, int sheetNum, int colNum)
+    {
+        for(int sheets = 0; sheets < sheetNum; sheets++)
+        {
+            for(int columns = 0; columns < colNum; columns++)
+            {
+                workbook.getSheetAt(sheets).autoSizeColumn(columns);
+            }
+        }
+    }
+
 
 }
