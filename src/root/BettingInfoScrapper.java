5b26737644c917e8bd38984246edac785ca36233
Artem Sliusarenko <sliusarenko.artem@gmail.com>
2019-08-09 15:28:33 +0300

    Scrapping win-lose odds. Encountered an issue, website class names clash. Developers used same class names for different sport tables, made it hard to differentiate between sports.

--- src/root/BettingInfoScrapper.java
+++ src/root/BettingInfoScrapper.java
@@ -15,10 +15,7 @@ import java.util.ArrayList;
 import java.util.TreeMap;
 
 public class BettingInfoScrapper {
-//    //private static ArrayList<String[]> soccer = new ArrayList<>();
-//    private static ArrayList<String[]> basketball = new ArrayList<>();
-//    private static ArrayList<String[]> tennis = new ArrayList<>();
-//    private static ArrayList<String[]> hockey = new ArrayList<>();
+
     private static XSSFWorkbook workbook = new XSSFWorkbook();
     private static XSSFSheet sheet;
     private static final String url = "https://hintwise.com/";
@@ -40,7 +37,7 @@ public class BettingInfoScrapper {
         //sheet = workbook.createSheet("Hockey");
         readInformation("#hockey");
 
-        autisizeColumns(workbook,4,4);
+        autisizeColumns(workbook,4,8);
 
         try
         {
@@ -97,7 +94,8 @@ public class BettingInfoScrapper {
         * Fill first row of a sheet with column headers
          */
         row = sheet.createRow(rowNum++);
-        objArr = new Object[] {"Time", "Home vs Away", "League","Prediction"};
+        objArr = new Object[] {"Time", "Home vs Away", "League","Prediction", "Last 5 Home", "Last 5 Away",
+                " Odds Home Win", "Odds Away Win" };
         for (Object obj : objArr)
         {
             cell = row.createCell(cellNum++);
@@ -108,7 +106,7 @@ public class BettingInfoScrapper {
         objArr = null;
 
         System.out.println("Read and write " + sportType.replace("#","") + " data!");
-        String[] elementRow = new String[4] ;
+        String[] elementRow = new String[8];
 
 
 
@@ -127,6 +125,46 @@ public class BettingInfoScrapper {
                     final String teams = rowElement.select(".cellTEAMS.cellStyle").text();
                     final String division = rowElement.select(".hidden-xs.cellStyle").text();
 
+                    String lastFiveHome = "";
+                    String lastFiveAway = "";
+                    Double oddsHomeWin = 0.00;
+                    Double oddsAwayWin = 0.00;
+                    Double oddsDraw = 0.00;
+                    switch (sportType.replace("#", "")){
+                        case "soccer":
+                            oddsHomeWin = Double.parseDouble(rowElement.select(".homewin.winLose_Soccer.cellOdds input.btn.btn-xs.btn-info.btn-addToBlog").attr("value"));
+                            oddsAwayWin = Double.parseDouble(rowElement.select("td.winLose_Soccer.cellOdds:nth-of-type(19) input.btn.btn-xs.btn-info.btn-addToBlog").attr("value"));
+                            break;
+
+                        case "basketball":
+
+                            break;
+
+
+                        case "tennis":
+                            System.out.println(rowElement.select("div.tennis td.cellOdds.winLose.homewin input.btn.btn-xs.btn-default.btn-addToBlog").attr("value"));
+                            //oddsHomeWin = Double.parseDouble(rowElement.select("div.tennis td.cellOdds.winLose.homewin input.btn.btn-xs.btn-default.btn-addToBlog").attr("value"));
+                            //oddsAwayWin = Double.parseDouble(rowElement.select("td.winLose.cellOdds:nth-of-type(11) input.btn.btn-xs.btn-info.btn-addToBlog").attr("value"));
+//                            final String lastFiveHomeLose = rowElement.select("td.center.cellLast:nth-of-type(8)  div.winline-style.lose-background").text();
+//                            final String lastFiveHomeWin = rowElement.select("td.center.cellLast:nth-of-type(8)  div.winline-style.win-background").text();
+//                            lastFiveHome = lastFiveHome.concat(lastFiveHomeLose).concat(lastFiveHomeWin);
+//
+//                            final String lastFiveAwayLose = rowElement.select("td.center.cellLast:nth-of-type(9)  div.winline-style.lose-background").text();
+//                            final String lastFiveAwayWin = rowElement.select("td.center.cellLast:nth-of-type(9) div.winline-style.win-background").text();
+//                            lastFiveAway = lastFiveAway.concat(lastFiveAwayLose).concat(lastFiveAwayWin);
+                            break;
+
+                        case "hockey":
+
+                            break;
+
+                    }
+
+
+
+                    //final Double oddsAwayWin = Double.parseDouble(rowElement.select("td.winLose.cellOdds:nth-of-type(11)").text());
+
+
                     String prediction = rowElement.select(".cellStyle.lr20.cell90").text();
                     prediction = prediction.replace("Away", "A");
                     prediction = prediction.replace("Draw", "D");
@@ -136,6 +174,12 @@ public class BettingInfoScrapper {
                     elementRow[1] = teams;
                     elementRow[2] = division;
                     elementRow[3] = prediction;
+                    //elementRow[4] = lastFiveHome;
+                    //elementRow[5] = lastFiveAway;
+                    elementRow[6] = oddsHomeWin.toString();
+                    elementRow[7] = oddsAwayWin.toString();
+
+
 
                     row = sheet.createRow(rowNum++);
 
