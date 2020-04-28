6e8e7da7d584b6f64fbaa2e98b8f4a7af597736b
Artem Sliusarenko <sliusarenko.artem@gmail.com>
2020-04-28 17:13:31 +0300

    rebase testing no 2

--- src/root/ReadHtml.java
+++ src/root/ReadHtml.java
@@ -75,7 +75,7 @@ public class ReadHtml {
                     case "basketball":
                         // TODO parse basketball games info 
                         System.out.println("Basketball");
-                        // rebase testing
+                        // rebase testing No 2
                         break;
                     case "tennis":
                         //System.out.println(rowElement.select("div.tennis td.cellOdds.winLose.homewin input.btn.btn-xs.btn-default.btn-addToBlog").attr("value"));
